38865fd9c79482aa292f2cecf67b4c3cc16c6a5d
orangeCatDeveloper <mrlemon00@163.com>
2023-03-18 20:51:43 -0400

    a9 Mongo sp23

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import HelloController
     from "./controllers/hello-controller.js";
 import UserController
     from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+
+dotenv.config();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(cors());
